990d425c6178224d24a7fbf782b28925972ba5ed
Warren Kozak <warren.john.kozak@gmail.com>
2024-01-30 22:22:02 -0500

    Fix sql query so it actually works

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -9,14 +9,16 @@ conn = psycopg2.connect(
     user = "kozakw",
     password = "summer862winter",
 )
-cur = con.cursor
+cur = conn.cursor()
 
 def query_one():
-    cur.execute('SELECT city_name FROM cities WHERE city_name = "Northfield"')
+    cur.execute("SELECT city_name FROM cities WHERE city_name='Northfield'")
     result = cur.fetchone()
     
-    if result = None:
+    if result == None:
         print("Northfield is not found in the database.")
         return
 
     print(result)
+
+query_one()

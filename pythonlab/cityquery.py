73d5d10a76c5ec0da1bce1eb123a2112ec842fcf
Warren Kozak <warren.john.kozak@gmail.com>
2024-01-30 22:40:21 -0500

    Finish query two

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -12,21 +12,21 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 def query_one():
-    cur.execute("SELECT city_name FROM cities WHERE city_name='Northfield'")
+    cur.execute("SELECT city_name FROM cities WHERE city_name='Northfield';")
     result = cur.fetchone()
     
     if result == None:
         print("Northfield is not found in the database.")
         return
 
-    cur.execute("SELECT city_lat,city_lon FROM cities WHERE city_name='Northfield'")
+    cur.execute("SELECT city_lat,city_lon FROM cities WHERE city_name='Northfield';")
     location = cur.fetchone()
     print("lat: " + str(location[0]) + ", lon: " + str(location[1]))
 
 def query_two():
-    cur.execute("SELECT TOP 1 city_name FROM cities ORDER BY city_pop")
+    cur.execute("SELECT city_name FROM cities ORDER BY city_pop DESC;")
     largest_city = cur.fetchone()
-    print(largest_city)
+    print("Largest City: " + str(largest_city[0]))
 
 query_one()
 query_two()

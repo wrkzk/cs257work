27b1ccf250ea5002d2969409464835fac38bdfc2
Warren Kozak <wrkzk@protonmail.com>
2024-02-07 03:27:17 -0600

    Add files for more sql activity

---
+++ moresql/createtables.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import psycopg2
+
+def create_tables():
+
+    command1 = """
+        CREATE TABLE state_pops ( 
+            state_id text,
+            state_name text NOT NULL,
+            state_pop int NOT NULL
+        )
+        """
+    conn = None
+
+    try:
+        # Connect to server and create a cursor
+        conn = psycopg2.connect(
+            host = "localhost",
+            port = 5432,
+            database = "kozakw",
+            user = "kozakw",
+            password = "summer862winter",
+        )
+        cur = conn.cursor()
+
+        # Create each table
+        cur.execute(command1)
+
+        # Close connect to server and commit changes
+        cur.close()
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+if __name__ == '__main__':
+    create_tables()
